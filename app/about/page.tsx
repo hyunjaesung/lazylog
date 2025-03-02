f47ed37c158ad3af5d6b4bf0bfbafe07c9d2d88d
Steve Sung <stevysung@gmail.com>
2025-03-02 10:23:04 +0900

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -49,10 +49,8 @@ export default async function AboutPage({ params }: PageProps) {
   }
 
   return (
-    <article className="py-6 prose dark:prose-invert">
-      <h1>{page.title}</h1>
-      {page.description && <p className="text-xl">{page.description}</p>}
-      <hr />
+    <article className="py-6 prose dark:prose-invert w-full">
+      <h1 className="mb-2">{page.title}</h1>
       <Mdx code={page.body.code} />
     </article>
   );
